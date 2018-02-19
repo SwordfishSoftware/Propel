56cfe36c0f176033691ed34a8ef78ed6685d0373
pedri <pedri@swordfish.co.za>
2018-02-19 08:56:38 +0200

    Updates spaces

--- generator/lib/builder/om/PHP5ObjectBuilder.php
+++ generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -4128,122 +4128,122 @@ abstract class ".$this->getClassname()." extends ".$parentClass." ";
 		\$affectedRows = 0; // initialize var to track total num of affected rows
 
 ";
-		if ($reloadOnInsert || $reloadOnUpdate) {
-			$script .= "
-			\$reloadObject = false;
+	if ($reloadOnInsert || $reloadOnUpdate) {
+		$script .= "
+		\$reloadObject = false;
 ";
-		}
+	}
 
-		if (count($table->getForeignKeys())) {
-
-			$script .= "
-			// We call the save method on the following object(s) if they
-			// were passed to this object by their coresponding set
-			// method.  This object relates to these object(s) by a
-			// foreign key reference.
-";
-
-			foreach ($table->getForeignKeys() as $fk) {
-				$aVarName = $this->getFKVarName($fk);
-				$script .= "
-			if (\$this->$aVarName !== null) {
-				if (\$this->" . $aVarName . "->isModified() || \$this->" . $aVarName . "->isNew()) {
-					\$affectedRows += \$this->" . $aVarName . "->save(\$con);
-				}
-				\$this->set".$this->getFKPhpNameAffix($fk, $plural = false)."(\$this->$aVarName);
-			}
-";
-			} // foreach foreign k
-		} // if (count(foreign keys))
+	if (count($table->getForeignKeys())) {
 
 		$script .= "
-			if (\$this->isNew() || \$this->isModified()) {
-				// persist changes
-				if (\$this->isNew()) {
-					\$this->doInsert(\$con);";
-		if ($reloadOnInsert) {
+		// We call the save method on the following object(s) if they
+		// were passed to this object by their coresponding set
+		// method.  This object relates to these object(s) by a
+		// foreign key reference.
+";
+
+		foreach ($table->getForeignKeys() as $fk) {
+			$aVarName = $this->getFKVarName($fk);
 			$script .= "
-					if (!\$skipReload) {
-						\$reloadObject = true;
-					}";
+		if (\$this->$aVarName !== null) {
+			if (\$this->" . $aVarName . "->isModified() || \$this->" . $aVarName . "->isNew()) {
+				\$affectedRows += \$this->" . $aVarName . "->save(\$con);
+			}
+			\$this->set".$this->getFKPhpNameAffix($fk, $plural = false)."(\$this->$aVarName);
 		}
-		$script .= "
-				} else {
-					\$this->doUpdate(\$con);";
-		if ($reloadOnUpdate) {
-			$script .= "
-					if (!\$skipReload) {
-						\$reloadObject = true;
-					}";
-		}
-		$script .= "
-				}
-				\$affectedRows += 1;";
-
-		// We need to rewind any LOB columns
-		foreach ($table->getColumns() as $col) {
-			$clo = strtolower($col->getName());
-			if ($col->isLobType()) {
-				$script .= "
-				// Rewind the $clo LOB column, since PDO does not rewind after inserting value.
-				if (\$this->$clo !== null && is_resource(\$this->$clo)) {
-					rewind(\$this->$clo);
-				}
 ";
-			}
-		}
+		} // foreach foreign k
+	} // if (count(foreign keys))
 
+	$script .= "
+		if (\$this->isNew() || \$this->isModified()) {
+			// persist changes
+			if (\$this->isNew()) {
+				\$this->doInsert(\$con);";
+	if ($reloadOnInsert) {
 		$script .= "
-				\$this->resetModified();
-			}
-";
-
-		if ($table->hasCrossForeignKeys()) {
-			foreach ($table->getCrossFks() as $fkList) {
-				list($refFK, $crossFK) = $fkList;
-				$this->addCrossFkScheduledForDeletion($script, $refFK, $crossFK);
-			}
-		}
-
-		foreach ($table->getReferrers() as $refFK) {
-			$this->addRefFkScheduledForDeletion($script, $refFK);
-
-			if ($refFK->isLocalPrimaryKey()) {
-				$varName = $this->getPKRefFKVarName($refFK);
-				$script .= "
-			if (\$this->$varName !== null) {
-				if (!\$this->{$varName}->isDeleted()) {
-						\$affectedRows += \$this->{$varName}->save(\$con);
-				}
-			}
-";
+				if (!\$skipReload) {
+					\$reloadObject = true;
+				}";
+	}
+	$script .= "
 			} else {
-				$collName = $this->getRefFKCollVarName($refFK);
-				$script .= "
-			if (\$this->$collName !== null) {
-				foreach (\$this->$collName as \$referrerFK) {
-					if (!\$referrerFK->isDeleted()) {
-						\$affectedRows += \$referrerFK->save(\$con);
-					}
-				}
-			}
-";
-			} // if refFK->isLocalPrimaryKey()
-
-		} /* foreach getReferrers() */
-
+				\$this->doUpdate(\$con);";
+	if ($reloadOnUpdate) {
 		$script .= "
-			\$this->alreadyInSave = false;
-";
-		if ($reloadOnInsert || $reloadOnUpdate) {
+				if (!\$skipReload) {
+					\$reloadObject = true;
+				}";
+	}
+	$script .= "
+			}
+			\$affectedRows += 1;";
+
+	// We need to rewind any LOB columns
+	foreach ($table->getColumns() as $col) {
+		$clo = strtolower($col->getName());
+		if ($col->isLobType()) {
 			$script .= "
-			if (\$reloadObject) {
-				\$this->reload(\$con);
+			// Rewind the $clo LOB column, since PDO does not rewind after inserting value.
+			if (\$this->$clo !== null && is_resource(\$this->$clo)) {
+				rewind(\$this->$clo);
 			}
 ";
 		}
+	}
+
+	$script .= "
+			\$this->resetModified();
+		}
+";
+
+	if ($table->hasCrossForeignKeys()) {
+		foreach ($table->getCrossFks() as $fkList) {
+			list($refFK, $crossFK) = $fkList;
+			$this->addCrossFkScheduledForDeletion($script, $refFK, $crossFK);
+		}
+	}
+
+	foreach ($table->getReferrers() as $refFK) {
+		$this->addRefFkScheduledForDeletion($script, $refFK);
+
+		if ($refFK->isLocalPrimaryKey()) {
+			$varName = $this->getPKRefFKVarName($refFK);
+			$script .= "
+		if (\$this->$varName !== null) {
+			if (!\$this->{$varName}->isDeleted()) {
+					\$affectedRows += \$this->{$varName}->save(\$con);
+			}
+		}
+";
+		} else {
+			$collName = $this->getRefFKCollVarName($refFK);
+			$script .= "
+		if (\$this->$collName !== null) {
+			foreach (\$this->$collName as \$referrerFK) {
+				if (!\$referrerFK->isDeleted()) {
+					\$affectedRows += \$referrerFK->save(\$con);
+				}
+			}
+		}
+";
+		} // if refFK->isLocalPrimaryKey()
+
+	} /* foreach getReferrers() */
+
+	$script .= "
+		\$this->alreadyInSave = false;
+";
+	if ($reloadOnInsert || $reloadOnUpdate) {
 		$script .= "
-		return \$affectedRows;
+		if (\$reloadObject) {
+			\$this->reload(\$con);
+		}
+";
+	}
+	$script .= "
+	return \$affectedRows;
 	} // doSave()
 ";
 
@@ -4607,102 +4607,102 @@ abstract class ".$this->getClassname()." extends ".$parentClass." ";
 		if (!\$this->alreadyInSave) {
 			\$this->alreadyInSave = true;
 
-		if (\$this->isDeleted()) {
-			throw new PropelException(\"You cannot save an object that has been deleted.\");
-		}
-
-		if (\$con === null) {
-			\$con = Propel::getConnection(".$this->getPeerClassname()."::DATABASE_NAME, Propel::CONNECTION_WRITE);
-		}
-
-		\$con->beginTransaction();
-		\$isInsert = \$this->isNew();
-		try {";
-
-		if($this->getGeneratorConfig()->getBuildProperty('addHooks')) {
-			// save with runtime hools
-			$script .= "
-			\$ret = \$this->preSave(\$con);";
-			$this->applyBehaviorModifier('preSave', $script, "			");
-			$script .= "
-			if (\$isInsert) {
-				\$ret = \$ret && \$this->preInsert(\$con);";
-			$this->applyBehaviorModifier('preInsert', $script, "				");
-			$script .= "
-			} else {
-				\$ret = \$ret && \$this->preUpdate(\$con);";
-			$this->applyBehaviorModifier('preUpdate', $script, "				");
-			$script .= "
+			if (\$this->isDeleted()) {
+				throw new PropelException(\"You cannot save an object that has been deleted.\");
 			}
-			if (\$ret) {
-				\$affectedRows = \$this->doSave(\$con".($reloadOnUpdate || $reloadOnInsert ? ", \$skipReload" : "").");
+
+			if (\$con === null) {
+				\$con = Propel::getConnection(".$this->getPeerClassname()."::DATABASE_NAME, Propel::CONNECTION_WRITE);
+			}
+
+			\$con->beginTransaction();
+			\$isInsert = \$this->isNew();
+			try {";
+
+			if($this->getGeneratorConfig()->getBuildProperty('addHooks')) {
+				// save with runtime hools
+				$script .= "
+				\$ret = \$this->preSave(\$con);";
+				$this->applyBehaviorModifier('preSave', $script, "			");
+				$script .= "
 				if (\$isInsert) {
-					\$this->postInsert(\$con);";
-			$this->applyBehaviorModifier('postInsert', $script, "					");
-			$script .= "
-				} else {
-					\$this->postUpdate(\$con);";
-			$this->applyBehaviorModifier('postUpdate', $script, "					");
-			$script .= "
-				}
-				\$this->postSave(\$con);";
-				$this->applyBehaviorModifier('postSave', $script, "				");
-				$script .= "
-				".$this->getPeerClassname()."::addInstanceToPool(\$this);
-			} else {
-				\$affectedRows = 0;
-			}
-			\$con->commit();
-			return \$affectedRows;";
-		} else {
-			// save without runtime hooks
-			$this->applyBehaviorModifier('preSave', $script, "			");
-			if ($this->hasBehaviorModifier('preUpdate'))
-			{
-				$script .= "
-			if(!\$isInsert) {";
-				$this->applyBehaviorModifier('preUpdate', $script, "				");
-				$script .= "
-			}";
-			}
-			if ($this->hasBehaviorModifier('preInsert'))
-			{
-				$script .= "
-			if(\$isInsert) {";
+					\$ret = \$ret && \$this->preInsert(\$con);";
 				$this->applyBehaviorModifier('preInsert', $script, "				");
 				$script .= "
-			}";
+				} else {
+					\$ret = \$ret && \$this->preUpdate(\$con);";
+				$this->applyBehaviorModifier('preUpdate', $script, "				");
+				$script .= "
+				}
+				if (\$ret) {
+					\$affectedRows = \$this->doSave(\$con".($reloadOnUpdate || $reloadOnInsert ? ", \$skipReload" : "").");
+					if (\$isInsert) {
+						\$this->postInsert(\$con);";
+				$this->applyBehaviorModifier('postInsert', $script, "					");
+				$script .= "
+					} else {
+						\$this->postUpdate(\$con);";
+				$this->applyBehaviorModifier('postUpdate', $script, "					");
+				$script .= "
+					}
+					\$this->postSave(\$con);";
+					$this->applyBehaviorModifier('postSave', $script, "				");
+					$script .= "
+					".$this->getPeerClassname()."::addInstanceToPool(\$this);
+				} else {
+					\$affectedRows = 0;
+				}
+				\$con->commit();
+				return \$affectedRows;";
+			} else {
+				// save without runtime hooks
+				$this->applyBehaviorModifier('preSave', $script, "			");
+				if ($this->hasBehaviorModifier('preUpdate'))
+				{
+					$script .= "
+				if(!\$isInsert) {";
+					$this->applyBehaviorModifier('preUpdate', $script, "				");
+					$script .= "
+				}";
+				}
+				if ($this->hasBehaviorModifier('preInsert'))
+				{
+					$script .= "
+				if(\$isInsert) {";
+					$this->applyBehaviorModifier('preInsert', $script, "				");
+					$script .= "
+				}";
+				}
+				$script .= "
+				\$affectedRows = \$this->doSave(\$con".($reloadOnUpdate || $reloadOnInsert ? ", \$skipReload" : "").");";
+				$this->applyBehaviorModifier('postSave', $script, "			");
+				if ($this->hasBehaviorModifier('postUpdate'))
+				{
+					$script .= "
+				if(!\$isInsert) {";
+					$this->applyBehaviorModifier('postUpdate', $script, "				");
+					$script .= "
+				}";
+				}
+				if ($this->hasBehaviorModifier('postInsert'))
+				{
+					$script .= "
+				if(\$isInsert) {";
+					$this->applyBehaviorModifier('postInsert', $script, "				");
+					$script .= "
+				}";
+				}
+				$script .= "
+				\$con->commit();
+				".$this->getPeerClassname()."::addInstanceToPool(\$this);
+				return \$affectedRows;";
 			}
-			$script .= "
-			\$affectedRows = \$this->doSave(\$con".($reloadOnUpdate || $reloadOnInsert ? ", \$skipReload" : "").");";
-			$this->applyBehaviorModifier('postSave', $script, "			");
-			if ($this->hasBehaviorModifier('postUpdate'))
-			{
-				$script .= "
-			if(!\$isInsert) {";
-				$this->applyBehaviorModifier('postUpdate', $script, "				");
-				$script .= "
-			}";
-			}
-			if ($this->hasBehaviorModifier('postInsert'))
-			{
-				$script .= "
-			if(\$isInsert) {";
-				$this->applyBehaviorModifier('postInsert', $script, "				");
-				$script .= "
-			}";
-			}
-			$script .= "
-			\$con->commit();
-			".$this->getPeerClassname()."::addInstanceToPool(\$this);
-			return \$affectedRows;";
-		}
 
-		$script .= "
-		} catch (Exception \$e) {
-			\$con->rollBack();
-			throw \$e;
-		}
+			$script .= "
+			} catch (Exception \$e) {
+				\$con->rollBack();
+				throw \$e;
+			}
 		}";
 	}
 
