e40c69f092093e271618ad13200bf850482241ef
Ruben Filter <ruben@swordfish.co.za>
2018-09-04 10:23:06 +0200

    Allow php7.x to generate php5 compatible code for type double with default '0'
    When generating classes in php7.x the output was different when compared
    to the php5.5 generated code resulting in failure when running the code
    on the old interpreter.
    
    The fix might not be needed once all infrastructure is on 7.x, but for
    compatible switches between php versions during the migration phase
    it makes sense to keep the generated output in sync independent of the
    php version used to generate.

--- generator/lib/builder/om/PHP5ObjectBuilder.php
+++ generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -163,6 +163,15 @@ class PHP5ObjectBuilder extends ObjectBuilder
         } elseif ($col->isPhpPrimitiveType()) {
             settype($val, $col->getPhpType());
             $defaultValue = var_export($val, true);
+            /*
+             * Workaround for changes in var_export functioning in php 7.x when generating for 5.x
+             * If the default string is "0" but the type is double, then the output would be 0.0
+             * FIXME: Might have to revert this back to original state when using the result in 7.x
+             */
+            if ($defaultValue !== $val && $col->getPhpType() == 'double') {
+                settype($val, "int");
+                $defaultValue = var_export($val, true);
+            }
         } elseif ($col->isPhpObjectType()) {
             $defaultValue = 'new '.$col->getPhpType().'(' . var_export($val, true) . ')';
         } elseif ($col->isPhpArrayType()) {
